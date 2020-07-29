634f85a87a0f9cf7144f2e8cf3e510b21838fb8e
lilou <michellisa.75@gmail.com>
2020-07-29 11:16:40 +0200

    add update profile

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,7 +44,7 @@ class User implements UserInterface
     private $photo;
 
     /**
-     * @ORM\Column(type="string", length=60, nullable=true)
+     * @ORM\Column(type="string", length=60,  unique=true)
      */
     private $email;
 
@@ -111,6 +111,11 @@ class User implements UserInterface
         return $this->getUsername();
     }
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
     /**
      * A visual identifier that represents this user.
      *
@@ -118,7 +123,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->username;
+        return (string)$this->username;
     }
 
     public function setUsername(string $username): self
@@ -152,7 +157,7 @@ class User implements UserInterface
      */
     public function getPassword(): string
     {
-        return (string) $this->password;
+        return (string)$this->password;
     }
 
     public function setPassword(string $password): self
